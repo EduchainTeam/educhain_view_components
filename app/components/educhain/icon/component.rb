5c4cc8c94191c6b871dcb11230d547fb33d2f1ed
JuicyFlash <tankedet@bk.ru>
2025-02-14 00:31:44 +0300

    Добавлен гем tabler_icon_ruby Реализован компонент icon

--- app/components/educhain/icon/component.rb
+++ app/components/educhain/icon/component.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Educhain::Icon::Component < Educhain::BaseComponent
+  def initialize(name: "alert-triangle", size: 20, color: "black", filled: "false", stroke_width: 1)
+    @name = filled.to_s == "true" ? "#{name}-filled" : name
+    @size = size
+    @stroke_width = stroke_width
+    @color = color
+  end
+
+  def call
+    tabler_icon(@name, size: @size, color: @color, stroke_width: @stroke_width)
+  rescue
+    content_tag("div", "Не удалось загрузить иконку")
+  end
 end
