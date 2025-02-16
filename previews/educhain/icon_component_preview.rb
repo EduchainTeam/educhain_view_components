2df27416e94723bc5e2c2bb83447942f1c01bf85
JuicyFlash <tankedet@bk.ru>
2025-02-16 14:46:57 +0300

    213-улучшена работа с tailwind, добавлена команда bin/dev с автоматической пересборкой стилей

--- previews/educhain/icon_component_preview.rb
+++ previews/educhain/icon_component_preview.rb
@@ -3,6 +3,6 @@
 class Educhain::IconComponentPreview < ViewComponent::Preview
   # @label Icon
   def default()
-    render(Educhain::Icon::Component.new(class: 'w-50 h-50 bg-red-500'))
+    render(Educhain::Icon::Component.new(class: 'w-50 h-50 bg-green-500'))
   end
 end
