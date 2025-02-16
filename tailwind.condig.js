66daee0d61db377165be544094f3c91f67decc4c
MaratRakhimov <mf.rakhimov1982@gmail.com>
2025-02-16 18:57:45 +0300

    add tailwind

---
+++ tailwind.condig.js
@@ -0,0 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    './app/components/**/*.{erb,rb}'
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+};
