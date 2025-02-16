8f324b12e5379b9b5466d0dc4d3581755bdd5245
JuicyFlash <35424257+JuicyFlash@users.noreply.github.com>
2025-02-16 16:49:38 +0300

    Feature/213-icon-component (#2)
    * Добавлен гем tabler_icon_ruby
    Реализован компонент icon
    
    * Установлен tailwind
    Добавлена стилизация компонента (ошибка при загрузке иконки)
    
    * 213-удалён отладочный код
    
    * 213-удалён гем tabler_icon_ruby
    
    * 213-реализация компонента icon, добавлен svg файл с иконками, добавлен файл с названиями иконок, первичная реализация preview
    
    * 213-улучшена работа с tailwind, добавлена команда bin/dev с автоматической пересборкой стилей
    
    * 213-добавлны preview

--- educhain_view_components.gemspec
+++ educhain_view_components.gemspec
@@ -22,4 +22,5 @@ Gem::Specification.new do |spec|
   end
 
   spec.add_dependency "rails", ">= 7.2.2.1"
+  spec.add_dependency "tailwindcss-rails"
 end
--- previews/educhain/icon_component_preview.rb
+++ previews/educhain/icon_component_preview.rb
@@ -1,8 +1,25 @@
 # frozen_string_literal: true
 
 class Educhain::IconComponentPreview < ViewComponent::Preview
-  def default
-    render(Educhain::Icon::Component.new)
+  # Используются иконки ресурса https://remixicon.com/
+  # @label Default
+  # @param name text
+  # @param width number
+  # @param height number
+  # @param fill text
+  def default(name: "account-box-fill", width: 40, height: 40, fill: 'blue')
+    render(Educhain::Icon::Component.new(name:, width:, height:, fill:))
+  end
+
+  # К иконкам можно применять css стили через параметр class:
+  # @param name text
+  # @param class text
+  def stylable(name: "play-circle-fill", class: "w-10 h-10 hover:w-12 hover:h-12 hover:fill-blue-500")
+    render(Educhain::Icon::Component.new(name:, class:))
+  end
+
+  # @label All icons
+  def overview
+    render_with_template
   end
 end
-
