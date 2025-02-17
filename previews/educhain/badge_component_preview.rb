2b754da024628b233fa02db4001863090cf4079e
MaratRakhimov <mf.rakhimov1982@gmail.com>
2025-02-17 20:17:33 +0300

    add preview for badge

---
+++ previews/educhain/badge_component_preview.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Educhain::BadgeComponentPreview < ViewComponent::Preview
+  # @param name text
+  def overview(name: "Label")
+    render_with_template(locals: { name: })
+  end
+
+  # @param name text
+  # @param color select :color_options
+  # @param size select :size_options
+  def playground(name: "Label", color: :green, size: :m)
+    render current_component.new(name:, color:, size:)
+  end
+
+  private
+
+  def size_options
+    current_component::SIZES.keys
+  end
+
+  def color_options
+    current_component::COLORS.keys
+  end
+end
