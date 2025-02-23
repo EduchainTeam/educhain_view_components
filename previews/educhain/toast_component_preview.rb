0fa6af8d25987badf25c7afb301e464634ae54b5
MaratRakhimov <mf.rakhimov1982@gmail.com>
2025-02-23 20:15:51 +0300

    219 add toast preview

---
+++ previews/educhain/toast_component_preview.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# @component "ui/toast"
+class Educhain::Toast::ComponentPreview < ViewComponent::Preview
+  def overview
+    render_with_template
+  end
+
+  # @param scheme select { choices: [default, error] }
+  # @param text text
+  # @param icon text
+  def playground(text: "Toast", scheme: :default, icon: "checkbox-circle-fill")
+    render component("ui/toast").new(text:, scheme:, icon:)
+  end
+end
