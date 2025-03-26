ef1b9915a7be128360c16527b557eadec475dd34
Yuri Beloborodov <yubeloborodov@gmail.com>
2025-03-26 09:10:24 +0500

    Fix: add include SolidusAdmin::VoidElementsHelper to BaseComponent

--- app/components/educhain/base_component.rb
+++ app/components/educhain/base_component.rb
@@ -3,6 +3,7 @@
 module Educhain
   class BaseComponent < ViewComponent::Base
     include Educhain::ViewComponents::ComponentsHelper
+    include Educhain::ViewComponents::VoidElementsHelper
 
     def icon_tag(name, **attrs)
       render Educhain::Icon::Component.new(name:, **attrs)
