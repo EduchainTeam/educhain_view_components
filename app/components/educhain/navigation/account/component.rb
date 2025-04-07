ea28d2b37404f44a28fe7ab326a1365161199447
Dmitry Zaguta <zdvbind@gmail.com>
2025-04-07 17:25:49 +0500

    234 delete user_icon, account_icon params from account component

--- app/components/educhain/navigation/account/component.rb
+++ app/components/educhain/navigation/account/component.rb
@@ -1,15 +1,7 @@
 # frozen_string_literal: true
 
 class Educhain::Navigation::Account::Component < Educhain::BaseComponent
-  def initialize(
-    user_icon: "user-smile-fill",
-    logout_icon: "logout-box-line",
-    user_label: "my@account.dev",
-    account_path: "#",
-    logout_path: "#",
-    logout_method: :delete)
-    @logout_icon = logout_icon
-    @user_icon = user_icon
+  def initialize(user_label:, account_path:, logout_path:, logout_method:)
     @user_label = user_label
     @account_path = account_path
     @logout_path = logout_path
