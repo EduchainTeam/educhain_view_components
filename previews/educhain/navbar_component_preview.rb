ba019fc59acd5be5eb145d2fa9b0b512794e6e94
Sergey <eeatoe@gmail.com>
2025-04-15 00:10:55 -0400

    235 profile picture attribute transfer moved to user structure

--- previews/educhain/navbar_component_preview.rb
+++ previews/educhain/navbar_component_preview.rb
@@ -1,33 +1,34 @@
 # frozen_string_literal: true
 
 class Educhain::NavbarComponentPreview < ViewComponent::Preview
-  UserStub = Struct.new(:name)
+  UserStub = Struct.new(:name, :avatar_url, keyword_init: true)
 
   def overview
     render Educhain::Navbar::Component.new(
       brand_logo_url: "educhain/view_components/logo.svg", 
-      current_user: nil,
-      avatar_url: nil, 
+      current_user: nil, 
       profile_url: "#",
       nav_links: default_nav_links
     )
   end
 
-  def with_user_without_avatar(current_user: UserStub.new("Andrey"))
+  def with_user_without_avatar
+    current_user = UserStub.new(name: "Andrey", avatar_url: nil)
+
     render Educhain::Navbar::Component.new(
       brand_logo_url: "educhain/view_components/logo.svg", 
-      current_user: current_user, 
-      avatar_url: nil,
+      current_user: current_user,
       profile_url: "#",
       nav_links: default_nav_links
     )
   end
 
-  def with_user_with_avatar(current_user: UserStub.new("Andrey"))
+  def with_user_with_avatar
+    current_user = UserStub.new(name: "Andrey", avatar_url: "educhain/view_components/test-avatar.png")
+    
     render Educhain::Navbar::Component.new(
-      brand_logo_url: "educhain/view_components/logo.svg", 
-      current_user: current_user, 
-      avatar_url: "educhain/view_components/test-avatar.png",
+      brand_logo_url: "educhain/view_components/logo.svg",
+      current_user: current_user,
       profile_url: "#",
       nav_links: default_nav_links
     )
@@ -35,6 +36,7 @@ class Educhain::NavbarComponentPreview < ViewComponent::Preview
 
   private
 
+  # Returns an array of default navigation links for the navbar component.
   def default_nav_links
     [{ name: "Каталог", href: "#" }, { name: "Контакты", href: "#" }, { name: "О нас", href: "#" }]
   end
