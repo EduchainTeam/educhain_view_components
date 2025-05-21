0365f8a4a181161ca5ecf79a4f8b0b499cdd5510
AndreyChim <chimitcyrenov@mail.ru>
2025-05-21 15:11:22 +0800

    Fix name of user_label

--- previews/educhain/navigation_component_preview.rb
+++ previews/educhain/navigation_component_preview.rb
@@ -54,7 +54,7 @@ class Educhain::NavigationComponentPreview < ViewComponent::Preview
   # @label An example of custom logo and items
   def overview(
     logo_path: "educhain/view_components/logo/gml.svg",
-    user_label: :user_label
+    user_label: "admin@example.com"
   )
     render_with_template(locals: { logo_path:, user_label:, items: CUSTOM_ITEMS })
   end
